3cd9d2aee7884f62334f15d8aa78ab03abe80eea
sinYa Iwasaki <iwasaki@sinproject.net>
2024-03-26 00:52:37 +0900

    Support resizing window #32

--- src/resize.rs
+++ src/resize.rs
@@ -1,7 +1,10 @@
 use bevy::prelude::*;
 use bevy::window::WindowResized;
 
-use crate::lines::{self, LineMarker, RectangleMarker};
+use crate::{
+    lines::{self, LineMarker, RectangleMarker},
+    shapes::{self, CircleMarker, CrossMarker},
+};
 
 pub fn resize_listener(
     mut commands: Commands,
@@ -9,10 +12,14 @@ pub fn resize_listener(
     windows: Query<&Window>,
     rectangle_query: Query<Entity, With<RectangleMarker>>,
     line_query: Query<Entity, With<LineMarker>>,
+    circle_query: Query<Entity, With<CircleMarker>>,
+    cross_query: Query<Entity, With<CrossMarker>>,
 ) {
     for event in events.read() {
         lines::draw_rectangle(&mut commands, &windows, &rectangle_query);
         lines::draw_lines(&mut commands, &windows, &line_query);
+
+        shapes::draw_shapes(&mut commands, &windows, &circle_query, &cross_query);
 
         println!("Window resized to: {} {}", event.width, event.height);
     }
--- src/shapes.rs
+++ src/shapes.rs
@@ -1,72 +1,116 @@
-// use bevy::prelude::*;
-// use bevy_prototype_lyon::prelude::*;
+use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
+
+use crate::{consts, lines, window};
 
 // use crate::{consts, window};
 
-// const MARK_MARGIN: f32 = 10.0;
-// const MARK_WIDTH: f32 = 35.0;
+const MARK_MARGIN: f32 = 10.0;
+const MARK_WIDTH: f32 = 35.0;
 
-// pub fn draw_circle(commands: &mut Commands, cell_size: f32, line_width: f32, color: Color) {
-//     let radius = (cell_size) / 2.0 - line_width - MARK_MARGIN;
-//     let offset_x = 1.0 * cell_size;
-//     let offset_y = -1.0 * cell_size; // 左に1マス分のオフセット
+#[derive(Component)]
+pub struct CircleMarker;
 
-//     let shape = shapes::Circle {
-//         radius,
-//         ..default()
-//     };
+pub fn draw_circle(
+    commands: &mut Commands,
+    query: &Query<Entity, With<CircleMarker>>,
+    cell_size: f32,
+    line_width: f32,
+    color: Color,
+    scale: f32,
+) {
+    query.iter().for_each(|entity| {
+        commands.entity(entity).despawn();
+    });
 
-//     commands.spawn((
-//         ShapeBundle {
-//             path: GeometryBuilder::build_as(&shape),
-//             spatial: SpatialBundle {
-//                 transform: Transform::from_xyz(offset_x, offset_y, 0.),
-//                 ..default()
-//             },
-//             ..default()
-//         },
-//         Stroke::new(color, line_width),
-//     ));
-// }
+    let line_width = line_width * scale;
+    let margin_scale = MARK_MARGIN * scale;
+    let radius = (cell_size) / 2.0 - line_width - margin_scale;
+    let offset_x = 1.0 * cell_size;
+    let offset_y = -1.0 * cell_size; // 左に1マス分のオフセット
 
-// pub fn draw_cross(commands: &mut Commands, cell_size: f32, line_width: f32, color: Color) {
-//     let half_size = (cell_size) / 2.0 - MARK_MARGIN - line_width * 3.0 / 4.0;
+    let shape = shapes::Circle {
+        radius,
+        ..default()
+    };
 
-//     // バツの形を正しく描画するために、開始点と終了点を修正します。
-//     let line_1_start = Vec2::new(half_size, half_size);
-//     let line_1_end = Vec2::new(-half_size, -half_size);
+    commands.spawn((
+        ShapeBundle {
+            path: GeometryBuilder::build_as(&shape),
+            spatial: SpatialBundle {
+                transform: Transform::from_xyz(offset_x, offset_y, 0.),
+                ..default()
+            },
+            ..default()
+        },
+        Stroke::new(color, line_width),
+        CircleMarker,
+    ));
+}
 
-//     let line_2_start = Vec2::new(half_size, -half_size);
-//     let line_2_end = Vec2::new(-half_size, half_size);
+#[derive(Component)]
+pub struct CrossMarker;
 
-//     commands.spawn((
-//         ShapeBundle {
-//             path: GeometryBuilder::build_as(&shapes::Line(line_1_start, line_1_end)),
-//             // spatial: SpatialBundle {
-//             //     transform: Transform::from_xyz(-offset_x, -offset_x, 0.),
-//             //     ..default()
-//             // },
-//             ..default()
-//         },
-//         Stroke::new(color, line_width),
-//     ));
-//     commands.spawn((
-//         ShapeBundle {
-//             path: GeometryBuilder::build_as(&shapes::Line(line_2_start, line_2_end)),
-//             // spatial: SpatialBundle {
-//             //     transform: Transform::from_xyz(-offset_x, -offset_x, 0.),
-//             //     ..default()
-//             // },
-//             ..default()
-//         },
-//         Stroke::new(color, line_width),
-//     ));
-// }
+pub fn draw_cross(
+    commands: &mut Commands,
+    query: &Query<Entity, With<CrossMarker>>,
+    cell_size: f32,
+    line_width: f32,
+    color: Color,
+    scale: f32,
+) {
+    query.iter().for_each(|entity| {
+        commands.entity(entity).despawn();
+    });
 
-// pub fn draw_shapes(mut commands: Commands, windows: Query<&Window>) {
-//     let (cell_size, _) = window::get_sizes(&windows);
-//     let color = consts::get_color();
+    let mark_margin = MARK_MARGIN * scale;
+    let line_width = line_width * scale;
+    let half_size = (cell_size) / 2.0 - mark_margin - line_width * 3.0 / 4.0;
 
-//     draw_circle(&mut commands, cell_size, MARK_WIDTH, color);
-//     draw_cross(&mut commands, cell_size, MARK_WIDTH, color);
-// }
+    // バツの形を正しく描画するために、開始点と終了点を修正します。
+    let line_1_start = Vec2::new(half_size, half_size);
+    let line_1_end = Vec2::new(-half_size, -half_size);
+
+    let line_2_start = Vec2::new(half_size, -half_size);
+    let line_2_end = Vec2::new(-half_size, half_size);
+
+    commands.spawn((
+        ShapeBundle {
+            path: GeometryBuilder::build_as(&shapes::Line(line_1_start, line_1_end)),
+            // spatial: SpatialBundle {
+            //     transform: Transform::from_xyz(-offset_x, -offset_x, 0.),
+            //     ..default()
+            // },
+            ..default()
+        },
+        Stroke::new(color, line_width),
+        CrossMarker,
+    ));
+    commands.spawn((
+        ShapeBundle {
+            path: GeometryBuilder::build_as(&shapes::Line(line_2_start, line_2_end)),
+            // spatial: SpatialBundle {
+            //     transform: Transform::from_xyz(-offset_x, -offset_x, 0.),
+            //     ..default()
+            // },
+            ..default()
+        },
+        Stroke::new(color, line_width),
+        CrossMarker,
+    ));
+}
+
+pub fn draw_shapes(
+    commands: &mut Commands,
+    windows: &Query<&Window>,
+    circle_query: &Query<Entity, With<CircleMarker>>,
+    cross_query: &Query<Entity, With<CrossMarker>>,
+) {
+    let (cell_size, _) = window::get_sizes(windows);
+    let color = consts::get_color();
+    let window_size = window::get_window_size(windows);
+    let scale = lines::get_scale(window_size);
+
+    draw_circle(commands, circle_query, cell_size, MARK_WIDTH, color, scale);
+    draw_cross(commands, cross_query, cell_size, MARK_WIDTH, color, scale);
+}
